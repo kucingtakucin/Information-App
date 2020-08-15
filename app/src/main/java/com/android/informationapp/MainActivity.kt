71f12cb1b43d26572d8e62cf118d8e7d1012d75e
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-15 19:05:38 +0700

    explicit intent ke activity lain

--- app/src/main/java/com/android/informationapp/MainActivity.kt
+++ app/src/main/java/com/android/informationapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.android.informationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.android.informationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        dataBinding.apply {
+            button.setOnClickListener {
+                val intent = Intent(this@MainActivity, ResultActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
