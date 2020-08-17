52ee890ed024f5ab0e7b7492207d8f33fd91c9b5
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-17 20:44:56 +0700

    efek rotasi & savedInstanceState

--- app/src/main/java/com/android/informationapp/ResultActivity.kt
+++ app/src/main/java/com/android/informationapp/ResultActivity.kt
@@ -4,29 +4,48 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.android.informationapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityResultBinding
+    private var counter: Int = 0
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = DataBindingUtil.setContentView(this@ResultActivity, R.layout.activity_result)
         dataBinding.apply {
-            Result.text = intent.getStringExtra("name") + "\n" + intent.getStringExtra("email")
-            button2.setOnClickListener {
+            resultText.text = intent.getStringExtra("name") + "\n" + intent.getStringExtra("email")
+            shareButton.setOnClickListener {
                 val sendIntent: Intent = Intent().apply {
                     action = Intent.ACTION_SEND
                     type = "text/plain"
-                    putExtra(Intent.EXTRA_TEXT, Result.text.toString())
+                    putExtra(Intent.EXTRA_TEXT, resultText.text.toString())
                 }
                 startActivity(sendIntent)
             }
+
+            if (savedInstanceState !== null) {
+                resultText.text = savedInstanceState.getString("COUNTER_NUMBER")
+                counter = savedInstanceState.getString("COUNTER_NUMBER")?.toInt()!!
+            }
+            upButton.setOnClickListener {
+                counter++
+                resultText.text = counter.toString()
+            }
+
         }
         Log.i("Lifecycle", "onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.run {
+            putString("COUNTER_NUMBER", counter.toString())
+        }
     }
 
     override fun onStart() {
